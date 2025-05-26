fd714fc71688dbde7b70c9e61ebd9e3c4e777a03
MihirChandra04 <mihirchandra04@gmail.com>
2025-05-26 09:15:07 +0530

    Remove __pycache__ and venv from repo and add .gitignore and added delete endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException,status
 from .model import FormData,AddressUpdate
 from .database import collection
 from pydantic import EmailStr
@@ -6,9 +6,9 @@ from pydantic import EmailStr
 router = APIRouter()
 
 @router.get("/user")
-async def get_data(email : dict):
+async def get_data(email : str):
     try:
-        data = collection.find_one({"email":email["email"]})
+        data = collection.find_one({"email":email})
         data["_id"] = str(data["_id"])
         return data
     except Exception as e:
@@ -42,5 +42,42 @@ async def submit_form(data: FormData):
         raise HTTPException(status_code=500, detail=str(e))
 
 
+@router.delete('/delete/{email}')
+async def delete_data(email: str):
+    try:
+        data =  collection.find_one({"email": email})
+        if not data:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Form data with email {email} not found"
+            )
+
+        result =  collection.delete_one({"email": email})
+        if result.deleted_count == 1:
+            return {"message": f"Form data with email {email} deleted successfully"}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to delete the document"
+            )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occurred: {str(e)}"
+        )
+
+
+@router.get("/all-users")
+async def get_all_users():
+    try:
+        data = []
+        for doc in collection.find():
+            doc["_id"] = str(doc["_id"])  # Make ObjectId JSON serializable
+            data.append(doc)
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"error occurred: {str(e)}")
+
+    
 # In Pydantic v1 → data.dict() was used
 # In Pydantic v2 → .model_dump() is more flexible, supports customization and is future-proof
